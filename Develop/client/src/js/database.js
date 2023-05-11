6d2bed0e58600a18a14a53efb4a796899090ce2e
Connor S <constroh@gmail.com>
2023-05-11 14:32:01 -0500

    syntax changes

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -20,7 +20,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore("jate");
   const request = store.put({ id: 1, vlaue: content });
   const result = await request;
-  console.log("Entry has been saved to the database!", result);
+  console.log("Updated text entry has been saved to the database!", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
